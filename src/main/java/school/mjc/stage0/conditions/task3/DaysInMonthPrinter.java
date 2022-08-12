9009fdae5b9b74ad60b105b66b32e30120f6f801
lucciola200 <teona.odisharia@gmail.com>
2022-08-12 16:04:53 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -7,6 +7,8 @@ public class DaysInMonthPrinter {
             System.out.println(31);
         } else if (month == 2) {
             System.out.println(28);
+        } else if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
         } else {
             System.out.println(30);
         }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -3,17 +3,16 @@ package school.mjc.stage0.conditions.task3;
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
 //        spring, summer, fall, and winter
-        if (month >= 6) {
-            System.out.println("summer");
-        } else if (month == 5) {
-            System.out.println("spring");
-        } else if (month == 4) {
-            System.out.println("winter");
-        } else if (month == 3) {
-            System.out.println("fall");
+        if (month >= 6 && month < 9) {
+            System.out.println("Summer");
+        } else if (month >= 3 && month < 6) {
+            System.out.println("Spring");
+        } else if (month == 12 || month == 1 || month == 2) {
+            System.out.println("Winter");
+        } else if (month >= 9 && month < 12) {
+            System.out.println("Fall");
         } else {
             System.out.println("wrong number!");
         }
-
     }
 }
