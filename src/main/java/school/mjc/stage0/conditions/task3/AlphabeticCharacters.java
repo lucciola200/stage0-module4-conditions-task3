5ee904780c5bca7e08765dfdfe7a619868a718ff
lucciola200 <teona.odisharia@gmail.com>
2022-08-12 15:55:05 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+            if ("AEIOUaeiou".indexOf(character) != -1) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+//        spring, summer, fall, and winter
+        if (month >= 6) {
+            System.out.println("summer");
+        } else if (month == 5) {
+            System.out.println("spring");
+        } else if (month == 4) {
+            System.out.println("winter");
+        } else if (month == 3) {
+            System.out.println("fall");
+        } else {
+            System.out.println("wrong number!");
+        }
 
     }
 }
